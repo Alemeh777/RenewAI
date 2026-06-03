97ed6b4ddf5d53bb2f8ac548bcf41aea08f9dd37
Alemeh <alemeh@MacBook-Pro-som-tillhor-adel.local>
2026-06-03 10:15:39 +0200

    Fix CSP for Clerk production

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: '',
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
