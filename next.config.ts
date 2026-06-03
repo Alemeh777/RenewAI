b7751b983443350bed3d08820ff34a4060e461ac
Alemeh <alemeh@MacBook-Pro-som-tillhor-adel.local>
2026-06-03 14:38:46 +0200

    Remove CSP headers

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.com https://*.clerk.accounts.dev https://*.ozhenai.com; worker-src 'self' blob:;",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
